789772d71ff3412cf3a61828dfa58f28edefb18a
Benjamin. L. Whelan <whelbe.g@gmail.com>
2020-05-15 16:29:02 +0200

    Quit/Resume/Options Buttons
    Basic buttons work. Still need to add menu connection. Also need to fix resume button ~ escape still works to pause.

---
+++ SpaceForce/Assets/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()   // resumes game
+    {
+        pauseMenuUI.SetActive(false);   // sets object inactive
+        Time.timeScale = 1f;         // sets time speed to 1x (standard)
+        GamePaused = false;             
+    }
+
+    void Pause()    // pauses game
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
