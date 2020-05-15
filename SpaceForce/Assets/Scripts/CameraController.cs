6912bd64d016bea923aa1fc98b8785cb86929a91
Damian <Damian.meijer@gmail.com>
2020-05-15 11:30:24 +0200

    CamMovement

--- SpaceForce/Assets/Scripts/CameraController.cs
+++ SpaceForce/Assets/Scripts/CameraController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float camSpeed = 20f;
+    public float panBorderthickness = 10f;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 currentPos = transform.position;
+
+
+
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderthickness)
+        {
+            currentPos.y += camSpeed * Time.deltaTime;
+        }
+         if(Input.GetKey("s") || Input.mousePosition.y <= panBorderthickness)
+        {
+            currentPos.y -= camSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderthickness)
+        {
+            currentPos.x += camSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey("a") || Input.mousePosition.x <= panBorderthickness)
+        {
+            currentPos.x -= camSpeed * Time.deltaTime;
+        }
+       
+        transform.position = currentPos;
     }
 }
