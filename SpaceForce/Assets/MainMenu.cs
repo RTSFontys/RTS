d7fa2ca8742529f8e526c1fb9df8eb2170c970c8
Benjamin. L. Whelan <whelbe.g@gmail.com>
2020-05-15 13:48:00 +0200

    Quit Button

--- SpaceForce/Assets/MainMenu.cs
+++ SpaceForce/Assets/MainMenu.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.Hosting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame ()
+    public void QuitGame()
     {
         Debug.Log("Quit");
-        Application.Quit(); 
+        Application.Quit();
     }
 }
