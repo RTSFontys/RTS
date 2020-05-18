a7de6129bcae69eef9999565f6941d457fc8344a
Damian <Damian.meijer@gmail.com>
2020-05-18 09:29:59 +0200

    Update menu/AddedPNG units (placeholders)

--- SpaceForce/Assets/PauseMenu.cs
+++ SpaceForce/Assets/PauseMenu.cs
@@ -43,7 +43,7 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void QuitGame()
